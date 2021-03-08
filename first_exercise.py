3aa96c71886a8af12930aefabe8eb50f61a31840
Plamen Delchev <plamen.delchev@gmail.com>
2021-03-08 20:51:39 +0200

    working, but poorly

--- first_exercise.py
+++ first_exercise.py
@@ -45,89 +45,64 @@ def is_valid(matrix, i, j):
 def find_size(matrix, i, j):
     # if the cell is invalid
     if not is_valid(matrix, i, j):
-        return None
+        return 0
 
     # construct a unique dictionary key from dynamic elements of the input
     key = (i, j)
 
     if key not in is_marked:
         is_marked[key] = True
+    elif key in is_marked and is_marked[key]:
+        return 0
     else:
-        return
+        is_marked[key] = True
 
-    # if the subproblem is seen for the first time, solve it and
-    # store its result in a dictionary
-    if key not in lookup:
+    # string to store size starting `(i, j)`
+    size = 0
+    temp_size = 0
 
-        # string to store size starting `(i, j)`
-        size = 0
-        temp_size = 0
+    # recur top cell if its value is +1 of value at `(i, j)`
+    if i > 0 and matrix[i - 1][j] == matrix[i][j]:
+        temp_size = find_size(matrix, i - 1, j)
+        if temp_size > size:
+            size = temp_size
 
-        # recur top cell if its value is +1 of value at `(i, j)`
-        if i > 0 and matrix[i - 1][j] == matrix[i][j]:
-            size = find_size(matrix, i - 1, j)
-            if not size:
-                size = 0
+    # recur right cell if its value is +1 of value at `(i, j)`
+    if j + 1 < len(matrix) and matrix[i][j + 1] == matrix[i][j]:
+        temp_size = find_size(matrix, i, j + 1)
+        if temp_size > size:
+            size = temp_size
 
-        # recur right cell if its value is +1 of value at `(i, j)`
-        if j + 1 < len(matrix) and matrix[i][j + 1] == matrix[i][j]:
-            temp_size = find_size(matrix, i, j + 1)
-            if temp_size:
-                if temp_size > size:
-                    size = temp_size
+    # recur bottom cell if its value is +1 of value at `(i, j)`
+    if i + 1 < len(matrix) and matrix[i + 1][j] == matrix[i][j]:
+        temp_size = find_size(matrix, i + 1, j)
+        if temp_size > size:
+            size = temp_size
 
-        # recur bottom cell if its value is +1 of value at `(i, j)`
-        if i + 1 < len(matrix) and matrix[i + 1][j] == matrix[i][j]:
-            temp_size = find_size(matrix, i + 1, j)
-            if temp_size:
-                if temp_size > size:
-                    size = temp_size
+    # recur left cell if its value is +1 of value at `(i, j)`
+    if j > 0 and matrix[i][j - 1] == matrix[i][j]:
+        temp_size = find_size(matrix, i, j - 1)
+        if temp_size > size:
+            size = temp_size
 
-        # recur left cell if its value is +1 of value at `(i, j)`
-        if j > 0 and matrix[i][j - 1] == matrix[i][j]:
-            temp_size = find_size(matrix, i, j - 1)
-            if temp_size:
-                if temp_size > size:
-                    size = temp_size
+    # note that as the matrix contains all distinct elements,
+    # there is only one size possible from the current cell
 
-        # note that as the matrix contains all distinct elements,
-        # there is only one size possible from the current cell
-
-        is_marked[key] = False
-        lookup[key] = size + 1 if size else 1
-    else:
-        # recur top cell if its value is +1 of value at `(i, j)`
-        if i > 0 and matrix[i - 1][j] == matrix[i][j]:
-            temp_size = find_size(matrix, i - 1, j)
-            if temp_size:
-                if temp_size > size:
-                    size = temp_size
-
-        # recur left cell if its value is +1 of value at `(i, j)`
-        if j > 0 and matrix[i][j - 1] == matrix[i][j]:
-            temp_size = find_size(matrix, i, j - 1)
-            if temp_size:
-                if temp_size > size:
-                    size = temp_size
-
-        is_marked[key] = False
-        lookup[key] = size + 1 if size else 1
+    is_marked[key] = False
+    size += 1
 
     # return size starting from `(i, j)`
-    return lookup[key]
+    return size
 
-lookup = {}
-is_marked = {}
 res_size = -1
+is_marked = {}
 
 # Find longest adjasent sequence in matrix
-for i in range(len(matrix)):
-    for j in range(len(matrix)):
-
+for i in range(rows):
+    for j in range(cols):
         s = find_size(matrix, i, j)
 
-        if s:
-            if s > res_size:
-                res_size = s
+        if s > res_size:
+            res_size = s
 
 print(res_size)
